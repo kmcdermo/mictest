81d173952ba6a7d09065ef9aac7279fadeefff7e
Kevin McDermott <kpm82@cornell.edu>
2016-09-13 14:30:05 +0200

    tuning macros

---
+++ tuning.C
@@ -0,0 +1,47 @@
+#include "TString.h"
+#include "TStyle.h"
+#include "TVirtualFitter.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TF1.h"
+
+void tuning()
+{
+  TString var   = "phi";
+  Int_t   layer = 9;
+  Float_t range = 5.0;
+  Float_t frac  = 0.8;
+  Float_t nsig  = 3.0;
+
+  gStyle->SetOptStat("ou");
+  gStyle->SetOptFit (0111);
+  TVirtualFitter::SetDefaultFitter("Minuit2");
+
+  TFile * file = TFile::Open("valtree.root");
+  TTree * tree = (TTree*)file->Get("fittree");
+
+  TH1F * hist = new TH1F(Form("h_%s_pull_lay%i",var.Data(),layer),Form("(%s_prop[%i]-%s_hit[%i])/#sigma_{%s[%i]}",var.Data(),layer,var.Data(),layer,var.Data(),layer),100,-range,range);
+  //  TH1F * hist = new TH1F(Form("h_%s_diff_lay%i",var.Data(),layer),Form("%s_prop[%i]-%s_hit[%i]",var.Data(),layer,var.Data(),layer),100,-range,range);
+  hist->Sumw2();
+
+  tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
+
+  TCanvas * canv = new TCanvas();
+  canv->cd();
+  hist->Scale(1.0/hist->Integral());
+  hist->SetMinimum(0.000001);
+  hist->Draw("EP");
+
+  TF1 * fit = new TF1("fit","gaus(0)",-range*frac,range*frac);
+  fit->SetParName(0,"N");
+  fit->SetParName(1,"#mu");
+  fit->SetParName(2,"#sigma");
+  fit->SetParLimits(2,0,10);
+  hist->Fit("fit","R0Q"); // for real testing, do not suppress output --> drop q
+
+  fit->Draw("same");
+
+  canv->SaveAs(Form("zphi/%s.png",hist->GetName()));
+}
---
+++ tuning_integral.C
@@ -0,0 +1,87 @@
+#include "TString.h"
+#include "TStyle.h"
+#include "TVirtualFitter.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TF1.h"
+
+#include <fstream>
+#include <iostream>
+
+void tuning_integral()
+{
+  TString var   = "z";
+  Int_t   layer = 9;
+  Float_t range = 6.0;
+  Int_t   nbins = 1001;
+
+  bool save = true;
+  ofstream yields(Form("zphi/%sintegrals.txt",var.Data()),std::ios_base::app);
+
+  TFile * file = TFile::Open("valtree.root");
+  TTree * tree = (TTree*)file->Get("fittree");
+
+  TH1F * hist = new TH1F("hist","hist",nbins,-range,range); hist->Sumw2();
+  tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
+
+  int onesigbins[2]   = {-1,-1}; int onesigints[2]   = {-1,-1}; float onesigvals[2]   = {-99.,-99.}; bool onesigma   = false; 
+  int twosigbins[2]   = {-1,-1}; int twosigints[2]   = {-1,-1}; float twosigvals[2]   = {-99.,-99.}; bool twosigma   = false; 
+  int threesigbins[2] = {-1,-1}; int threesigints[2] = {-1,-1}; float threesigvals[2] = {-99.,-99.}; bool threesigma = false;
+
+  int total = hist->Integral(0,1002);
+  for (int i = 0; i <= 501; i++) {
+    int beg = 501 - i;
+    int end = 501 + i;
+    
+    int integral = hist->Integral(beg,end);
+    if ((float(integral)/float(total) > 0.6826895) && !onesigma) {
+      onesigma = true;
+      onesigbins[0] = end - 1; onesigbins[1] = end;
+      onesigints[0] = hist->Integral(beg+1,end-1);
+      onesigints[1] = integral;
+      onesigvals[0] = hist->GetBinCenter(end-1);
+      onesigvals[1] = hist->GetBinCenter(end);
+    }
+    else if ((float(integral)/float(total) > 0.9544997) && !twosigma) {
+      twosigma = true;
+      twosigbins[0] = end - 1; twosigbins[1] = end;
+      twosigints[0] = hist->Integral(beg+1,end-1);
+      twosigints[1] = integral;
+      twosigvals[0] = hist->GetBinCenter(end-1);
+      twosigvals[1] = hist->GetBinCenter(end);
+    }
+    else if ((float(integral)/float(total) > 0.9973002) && !threesigma) {
+      threesigma = true;
+      threesigbins[0] = end - 1; threesigbins[1] = end;
+      threesigints[0] = hist->Integral(beg+1,end-1);
+      threesigints[1] = integral;
+      threesigvals[0] = hist->GetBinCenter(end-1);
+      threesigvals[1] = hist->GetBinCenter(end);
+    }
+  }
+
+  if (!save) {
+    std::cout << "layer: " << layer << " range: " << range << std::endl;
+    std::cout << total << std::endl << std::endl;
+    std::cout << onesigbins[0] << "-" << onesigbins[1] << std::endl;
+    std::cout << onesigints[0] << "-" << onesigints[1] << std::endl;
+    std::cout << onesigvals[0] << "-" << onesigvals[1] << std::endl << std::endl;
+    
+    std::cout << twosigbins[0] << "-" << twosigbins[1] << std::endl;
+    std::cout << twosigints[0] << "-" << twosigints[1] << std::endl;
+    std::cout << twosigvals[0] << "-" << twosigvals[1] << std::endl << std::endl;
+
+    std::cout << threesigbins[0] << "-" << threesigbins[1] << std::endl;
+    std::cout << threesigints[0] << "-" << threesigints[1] << std::endl;
+    std::cout << threesigvals[0] << "-" << threesigvals[1] << std::endl;
+  }
+  else {
+    yields << layer << " "
+	   << (onesigvals[0]+onesigvals[1])/2. << " "
+      	   << (twosigvals[0]+twosigvals[1])/2. << " "
+      	   << (threesigvals[0]+threesigvals[1])/2. << " "
+	   << std::endl;
+  }
+}
