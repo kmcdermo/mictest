04ff2a784aa6afd459685917eea560274573aff2
Kevin McDermott <kpm82@cornell.edu>
2016-09-16 11:04:29 -0700

    minor cahnges

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -2,10 +2,10 @@
 
 [ -z "$ROOTSYS" ] && source ~matevz/root/bin/thisroot.sh
 
-./benchmark.sh
-python makeBenchmarkPlots.py host
-root -b -q -l makeBenchmarkPlots.C
+#./benchmark.sh
+#python makeBenchmarkPlots.py host
+#root -b -q -l makeBenchmarkPlots.C
 
-./benchmark-mic.sh
+#./benchmark-mic.sh
 python makeBenchmarkPlots.py mic
 root -b -q -l makeBenchmarkPlots.C\(1\)
