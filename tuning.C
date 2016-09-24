771c4d4ab5fcc66e8cbef3c0cedde451ca919c18
Kevin McDermott <kpm82@cornell.edu>
2016-09-24 23:06:53 +0200

    update tuning macro

--- tuning.C
+++ tuning.C
@@ -7,41 +7,52 @@
 #include "TCanvas.h"
 #include "TF1.h"
 
+#include <fstream>
+
 void tuning()
 {
   TString var   = "phi";
-  Int_t   layer = 16;
-  Float_t range = 0.01;
+  Float_t range = 5.0;
   Float_t frac  = 0.8;
-  //  Float_t nsig  = 3.0;
 
   gStyle->SetOptStat("ou");
   gStyle->SetOptFit (0111);
   TVirtualFitter::SetDefaultFitter("Minuit2");
 
-  TFile * file = TFile::Open("cmsswvaltree.root");
+  TFile * file = TFile::Open("valtree.root");
   TTree * tree = (TTree*)file->Get("fittree");
 
-  TH1F * hist = new TH1F(Form("h_%s_diff_lay%i",var.Data(),layer),Form("%s_prop[%i]-%s_hit[%i]",var.Data(),layer,var.Data(),layer),100,-range,range); hist->Sumw2();
-  tree->Draw(Form("%s_prop[%i]-%s_hit[%i]>>%s",var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
-  
-//   TH1F * hist = new TH1F(Form("h_%s_pull_lay%i",var.Data(),layer),Form("(%s_prop[%i]-%s_hit[%i])/#sigma_{%s[%i]}",var.Data(),layer,var.Data(),layer,var.Data(),layer),100,-range,range); hist->Sumw2();
-//   tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
- 
-  TCanvas * canv = new TCanvas();
-  canv->cd();
-  hist->Scale(1.0/hist->Integral());
-  hist->SetMinimum(0.000001);
-  hist->Draw("EP");
+  ofstream output(Form("zphi_newsimple/%s_output.txt",var.Data()),std::ios::app);
 
-  TF1 * fit = new TF1("fit","gaus(0)",-range*frac,range*frac);
-  fit->SetParName(0,"N");
-  fit->SetParName(1,"#mu");
-  fit->SetParName(2,"#sigma");
-  fit->SetParLimits(2,0,10);
-  hist->Fit("fit","R0Q"); // for real testing, do not suppress output --> drop q
+  for (int i = 3; i < 10; i++)
+  {
+    Int_t layer = i;
 
-  fit->Draw("same");
+    TCanvas * canv = new TCanvas();
+    canv->cd();
 
-  canv->SaveAs(Form("zphicmssw/%s.png",hist->GetName()));
+    TH1F * hist = new TH1F(Form("h_%s_diff_lay%i",var.Data(),layer),Form("%s_prop[%i]-%s_hit[%i]",var.Data(),layer,var.Data(),layer),100,-range,range); hist->Sumw2();
+    tree->Draw(Form("%s_prop[%i]-%s_hit[%i]>>%s",var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
+    
+    hist->Scale(1.0/hist->Integral());
+    hist->SetMinimum(0.000001);
+    hist->Draw("EP");
+    
+    TF1 * fit = new TF1("fit","gaus(0)",-range*frac,range*frac);
+    fit->SetParName(0,"N");
+    fit->SetParName(1,"#mu");
+    fit->SetParName(2,"#sigma");
+    fit->SetParLimits(2,0,10);
+    hist->Fit("fit","R0Q"); // for real testing, do not suppress output --> drop q
+    
+    fit->Draw("same");
+
+    output << fit->GetParameter(2) << std::endl;
+
+    canv->SaveAs(Form("zphi_newsimple/%s.png",hist->GetName()));
+
+    delete fit;
+    delete hist;
+    delete canv;
+  }
 }
