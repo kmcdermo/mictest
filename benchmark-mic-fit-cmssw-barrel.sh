b3c9ebb898c7b7ff277b338976fcb3855845a35b
Kevin McDermott <kpm82@cornell.edu>
2016-09-24 06:25:19 -0700

    dumb mistakes in benchmarking for new fit on mic

--- benchmark-mic-fit-cmssw-barrel.sh
+++ benchmark-mic-fit-cmssw-barrel.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-dir=/data/nfsmic/cerati/
+dir=/nfsmic/cerati/
 
 patch < cmssw-config.patch
 
@@ -8,8 +8,8 @@ make -j 8
 
 for nth in 1 3 7 21 42 63 84 105 126 147 168 189 210
 do
-echo "host cmssw" nth=${nth} "FIT"
-./mkFit/mkFit --read --file-name ${dir}/cmssw_100xTTbarPU35_polar_split_mock_noFWD.bin --cms-geom --cmssw-seeds --fit-sort-only --num-thr ${nth} >& log_host_100xTTbarPU35_FIT_NVU8int_NTH${nth}.txt
+echo "mic cmssw" nth=${nth} "FIT"
+ssh mic0 ./mkFit-mic --read --file-name ${dir}/cmssw_100xTTbarPU35_polar_split_mock_noFWD.bin --cms-geom --cmssw-seeds --fit-sort-only --num-thr ${nth} >& log_mic_100xTTbarPU35_FIT_NVU16int_NTH${nth}.txt
 done
 
 sed -i 's/# USE_INTRINSICS := -DMPT_SIZE=1/USE_INTRINSICS := -DMPT_SIZE=XX/g' Makefile.config
@@ -18,8 +18,8 @@ do
 sed -i "s/MPT_SIZE=XX/MPT_SIZE=${nvu}/g" Makefile.config
 make clean
 make -j 8
-echo "host" nvu=${nvu} "FIT"
-./mkFit/mkFit --read --file-name ${dir}/cmssw_100xTTbarPU35_polar_split_mock_noFWD.bin --cms-geom --cmssw-seeds --fit-sort-only --num-thr 1 >& log_host_100xTTbarPU35_FIT_NVU${nvu}_NTH1.txt
+echo "mic" nvu=${nvu} "FIT"
+ssh mic0 ./mkFit-mic --read --file-name ${dir}/cmssw_100xTTbarPU35_polar_split_mock_noFWD.bin --cms-geom --cmssw-seeds --fit-sort-only --num-thr 1 >& log_mic_100xTTbarPU35_FIT_NVU${nvu}_NTH1.txt
 sed -i "s/MPT_SIZE=${nvu}/MPT_SIZE=XX/g" Makefile.config
 done
 sed -i 's/USE_INTRINSICS := -DMPT_SIZE=XX/# USE_INTRINSICS := -DMPT_SIZE=1/g' Makefile.config
