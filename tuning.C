4269ea8407e07df90e4a54bd0ac8f5d160cddce4
Kevin McDermott <kpm82@cornell.edu>
2016-09-26 17:53:11 +0200

    more zphi tuning tools

--- tuning.C
+++ tuning.C
@@ -1,47 +1,302 @@
-#include "TString.h"
-#include "TStyle.h"
-#include "TVirtualFitter.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TH1F.h"
-#include "TCanvas.h"
-#include "TF1.h"
+#include "tuning.h"
+
+namespace Config{
+  Double_t range = 0.01;
+  Double_t fitrange = range*0.8;
+  TString  formname = "gaus3fm";
+  Int_t    nbins = 50;
+  TString  histname;
+  TString  var   = "phi";
+  Int_t    layer = 16;
+  TString  outdir = "zphicmssw";
+};
 
 void tuning()
 {
-  TString var   = "phi";
-  Int_t   layer = 16;
-  Float_t range = 0.01;
-  Float_t frac  = 0.8;
-  //  Float_t nsig  = 3.0;
-
-  gStyle->SetOptStat("ou");
-  gStyle->SetOptFit (0111);
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(0);
   TVirtualFitter::SetDefaultFitter("Minuit2");
 
-  TFile * file = TFile::Open("cmsswvaltree.root");
+  // prep hist
+  TFile * file = TFile::Open("valtree_mateffects.root");
   TTree * tree = (TTree*)file->Get("fittree");
 
-  TH1F * hist = new TH1F(Form("h_%s_diff_lay%i",var.Data(),layer),Form("%s_prop[%i]-%s_hit[%i]",var.Data(),layer,var.Data(),layer),100,-range,range); hist->Sumw2();
-  tree->Draw(Form("%s_prop[%i]-%s_hit[%i]>>%s",var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
+  TH1F * hist = new TH1F(Form("h_%s_diff_lay%i",Config::var.Data(),Config::layer),
+			 Form("%s_prop[%i]-%s_hit[%i]",Config::var.Data(),Config::layer,Config::var.Data(),Config::layer),
+			 Config::nbins,-Config::range,Config::range); 
+  Config::histname = hist->GetName();
+  hist->Sumw2();
+
+  // fill the hist
+  tree->Draw(Form("%s_prop[%i]-%s_hit[%i]>>%s",Config::var.Data(),Config::layer,Config::var.Data(),Config::layer,Config::histname.Data()),
+	     Form("%s_hit[%i]!=-1000",Config::var.Data(),Config::layer),"goff");
   
-//   TH1F * hist = new TH1F(Form("h_%s_pull_lay%i",var.Data(),layer),Form("(%s_prop[%i]-%s_hit[%i])/#sigma_{%s[%i]}",var.Data(),layer,var.Data(),layer,var.Data(),layer),100,-range,range); hist->Sumw2();
-//   tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
- 
-  TCanvas * canv = new TCanvas();
-  canv->cd();
   hist->Scale(1.0/hist->Integral());
-  hist->SetMinimum(0.000001);
-  hist->Draw("EP");
+  hist->SetMarkerStyle(1);
+  hist->SetMarkerColor(kBlack);
+  hist->SetLineColor(kBlack);
 
-  TF1 * fit = new TF1("fit","gaus(0)",-range*frac,range*frac);
-  fit->SetParName(0,"N");
-  fit->SetParName(1,"#mu");
-  fit->SetParName(2,"#sigma");
-  fit->SetParLimits(2,0,10);
-  hist->Fit("fit","R0Q"); // for real testing, do not suppress output --> drop q
+  // perform the fit
+  fittingCore(hist);
 
+  // delete stuff
+  delete hist;
+  delete file;
+}
+
+void fittingCore(TH1F *& hist)
+{
+  // set up first canvas (linear)
+  TCanvas * lincanv = new TCanvas();
+  lincanv->SetLogy(0);
+  lincanv->cd();
+
+  Double_t mean, emean, sigma, esigma;
+  TF1 * fit;
+  arr3 temps;
+
+  // do the actual fit
+  doFit(hist,fit,temps);
+  getFitParams(fit,mean,emean,sigma,esigma);
+  hist->SetMaximum(0.3); 
+  hist->SetMinimum(0.0);
+  hist->Draw("ep");
+
+  // draw fit + subcomponents
+  TF1 * sub1; TF1 * sub2; TF1 * sub3;
+  drawFit(fit,lincanv,sub1,sub2,sub3);
+
+  // linear canvas only has mean, sigma, and chi2
+  TPaveText * text = new TPaveText(0.6,0.78,0.9,0.88,"NDC");
+  text->AddText(Form("#mu = %f #pm %f",mean,emean));
+  text->AddText(Form("#sigma = %f #pm %f",sigma,esigma));
+  text->AddText(Form("#chi^{2} / ndf = %6.2f / %i",fit->GetChisquare(),fit->GetNDF()));
+  text->SetTextSize(0.03);
+  text->SetFillColorAlpha(kWhite,0.f);
+  text->Draw("same");
+  
+  // save linear canvas
+  lincanv->SaveAs(Form("%s/%s_%s_lin.pdf",Config::outdir.Data(),Config::histname.Data(),Config::formname.Data()));
+
+  // log canvas saves individ. params
+  TCanvas * logcanv = new TCanvas();
+  logcanv->SetLogy(1);
+  logcanv->cd();
+
+  // redraw hist + fit
+  hist->SetMaximum(0.45);
+  hist->SetMinimum(0.0001);
+  hist->Draw("ep");
+  drawFit(fit,logcanv,sub1,sub2,sub3);
+
+  // draw individual params
+  TPaveText * stats;
+  drawStats(fit,stats,logcanv);
+
+  logcanv->SaveAs(Form("%s/%s_%s_log.pdf",Config::outdir.Data(),Config::histname.Data(),Config::formname.Data()));
+
+  // delete everything
+  deleteAll(fit,sub1,sub2,sub3,text,lincanv,stats,logcanv);
+}
+
+void doFit(TH1F *& hist, TF1 *& fit, arr3 & temps)
+{
+  TF1 * tempfit = new TF1("temp","gaus(0)",-Config::fitrange*0.5,Config::fitrange*0.5);
+  tempfit->SetParLimits(2,0,10);
+  hist->Fit("temp","RB0Q"); // for real testing, do not suppress output --> drop q
+  const Double_t norm  = tempfit->GetParameter(0); // constant
+  const Double_t mean  = tempfit->GetParameter(1); // mean
+  const Double_t sigma = tempfit->GetParameter(2); // sigma
+  temps = {norm,mean,sigma};
+  delete tempfit;
+
+  if (Config::formname.EqualTo("gaus1",TString::kExact)) {
+    TFormula form(Config::formname.Data(),"[0]*exp(-0.5*((x-[1])/[2])**2)");
+    fit  = new TF1(Form("%s_fit",Config::formname.Data()),Config::formname.Data(),-Config::fitrange,Config::fitrange);
+    fit->SetParName(0,"norm");  fit->SetParameter(0,norm);
+    fit->SetParName(1,"mean");  fit->SetParameter(1,mean);
+    fit->SetParName(2,"sigma"); fit->SetParameter(2,sigma);
+  }
+  else if (Config::formname.EqualTo("gaus2fm",TString::kExact)) {
+    TFormula form(Config::formname.Data(),"[0]*exp(-0.5*((x-[1])/[2])**2)+[3]*exp(-0.5*((x-[1])/[4])**2)");
+    fit  = new TF1(Form("%s_fit",Config::formname.Data()),Config::formname.Data(),-Config::fitrange,Config::fitrange);
+    fit->SetParName(0,"norm1");  fit->SetParameter(0,norm);  
+    fit->SetParName(1,"mean");   fit->SetParameter(1,mean); 
+    fit->SetParName(2,"sigma1"); fit->SetParameter(2,sigma);     fit->SetParLimits(2,0,10); 
+    fit->SetParName(3,"norm2");  fit->SetParameter(3,norm*0.1); 
+    fit->SetParName(4,"sigma2"); fit->SetParameter(4,sigma*4.0); fit->SetParLimits(4,0,10);
+  }
+  else if (Config::formname.EqualTo("gaus3fm",TString::kExact)) {
+    TFormula form(Config::formname.Data(),"[0]*exp(-0.5*((x-[1])/[2])**2)+[3]*exp(-0.5*((x-[1])/[4])**2)+[5]*exp(-0.5*((x-[1])/[6])**2)");
+    fit  = new TF1(Form("%s_fit",Config::formname.Data()),Config::formname.Data(),-Config::fitrange,Config::fitrange);
+    fit->SetParName(0,"norm1");  fit->SetParameter(0,norm*0.8);  //fit->SetParLimits(0,norm*0.5,norm);
+    fit->SetParName(1,"mean");   fit->SetParameter(1,mean);
+    fit->SetParName(2,"sigma1"); fit->SetParameter(2,sigma*0.7); fit->SetParLimits(2,0,10); //fit->SetParLimits(2,sigma*0.5,sigma);
+    fit->SetParName(3,"norm2");  fit->SetParameter(3,norm*0.3);  //fit->SetParLimits(3,norm*0.1,norm*0.5);
+    fit->SetParName(4,"sigma2"); fit->SetParameter(4,sigma*1.4); fit->SetParLimits(4,0,10); //fit->SetParLimits(4,sigma,sigma*1.5);
+    fit->SetParName(5,"norm3");  fit->SetParameter(5,norm*0.01); //fit->SetParLimits(5,norm*0.005,norm*0.1);
+    fit->SetParName(6,"sigma3"); fit->SetParameter(6,sigma*2.5); fit->SetParLimits(6,0,10); //fit->SetParLimits(6,sigma*1.5,sigma*5.0);
+  }
+  else if (Config::formname.EqualTo("crystalball",TString::kExact)) {
+    //  use directly the functionin ROOT::MATH note that the parameters definition is different is (alpha, n sigma, mu)
+    fit = new TF1(Form("%s_fit",Config::formname.Data()),Config::formname.Data(),-Config::fitrange,Config::fitrange);
+    fit->SetParameters(norm, mean, sigma, 1.0, 0.5);     // N, mean, sigma, alpha, n
+  }
+
+  Int_t status = hist->Fit(fit->GetName(),"RB0Q");
+}
+
+void getFitParams(TF1 *& fit, Double_t & mean, Double_t & emean, Double_t & sigma, Double_t & esigma) 
+{
+  if (Config::formname.EqualTo("gaus1",TString::kExact) || Config::formname.EqualTo("crystalball",TString::kExact)) {
+    mean   = fit->GetParameter(1);
+    sigma  = fit->GetParameter(2);
+
+    emean  = fit->GetParError(1);
+    esigma = fit->GetParError(2);
+  }
+  else if (Config::formname.EqualTo("gaus2fm",TString::kExact)) {
+    const Double_t const1 = fit->GetParameter(0); 
+    const Double_t const2 = fit->GetParameter(3);
+    const Double_t denom =  const1 + const2;
+
+    mean   = fit->GetParameter(1);
+    sigma  = (const1*fit->GetParameter(2) + const2*fit->GetParameter(4))/denom;
+
+    emean  = fit->GetParError(1);
+    esigma = rad2(const1*fit->GetParError(2),const2*fit->GetParError(4));
+
+    esigma = std::sqrt(esigma)/denom;
+  }
+  else if (Config::formname.EqualTo("gaus3fm",TString::kExact)) {
+    const Double_t const1 = fit->GetParameter(0); 
+    const Double_t const2 = fit->GetParameter(3);
+    const Double_t const3 = fit->GetParameter(5);
+    const Double_t denom  =  const1 + const2 + const3;
+
+    mean   = fit->GetParameter(1);
+    sigma  = (const1*fit->GetParameter(2) + const2*fit->GetParameter(4) + const3*fit->GetParameter(6))/denom;
+
+    emean  = fit->GetParError(1);
+    esigma = rad2(const1*fit->GetParError(2),const2*fit->GetParError(4))+std::pow(const3*fit->GetParError(6),2);
+
+    esigma = std::sqrt(esigma)/denom;
+  }
+}
+
+void dumpFit(TF1 *& fit, arr3 & temps)
+{
+  if (Config::formname.EqualTo("gaus3fm",TString::kExact)) {
+    std::cout << std::endl
+	      << " n1/n0: " << fit->GetParameter(0) / temps[0] << std::endl
+	      << " n2/n0: " << fit->GetParameter(3) / temps[0] << std::endl
+	      << " n3/n0: " << fit->GetParameter(5) / temps[0] << std::endl
+	      << " s1/s0: " << fit->GetParameter(2) / temps[2] << std::endl
+	      << " s2/s0: " << fit->GetParameter(4) / temps[2] << std::endl
+	      << " s3/s0: " << fit->GetParameter(6) / temps[2] << std::endl
+	      << " m-m0 : " << fit->GetParameter(1) - temps[1] << std::endl
+	      << std::endl;
+  }
+}
+
+void drawFit(TF1 *& fit, TCanvas *& canv, TF1 *& sub1, TF1 *& sub2, TF1 *& sub3) 
+{
+  canv->cd();
+
+  fit->SetLineWidth(3);
+  fit->SetLineColor(kMagenta-3); //kViolet-6
   fit->Draw("same");
 
-  canv->SaveAs(Form("zphicmssw/%s.png",hist->GetName()));
+  if (Config::formname.EqualTo("gaus1",TString::kExact) || Config::formname.EqualTo("crystalball",TString::kExact)) {
+    return;
+  }
+  else if (Config::formname.EqualTo("gaus2fm",TString::kExact)) {
+    sub1 = new TF1("sub1","gaus(0)",-Config::fitrange,Config::fitrange);
+    sub1->SetParameters(fit->GetParameter(0),fit->GetParameter(1),fit->GetParameter(2));
+  
+    sub2 = new TF1("sub2","gaus(0)",-Config::fitrange,Config::fitrange);
+    sub2->SetParameters(fit->GetParameter(3),fit->GetParameter(1),fit->GetParameter(4));
+  } 
+  else if (Config::formname.EqualTo("gaus3fm",TString::kExact)) {
+    sub1 = new TF1("sub1","gaus(0)",-Config::fitrange,Config::fitrange);
+    sub1->SetParameters(fit->GetParameter(0),fit->GetParameter(1),fit->GetParameter(2));
+  
+    sub2 = new TF1("sub2","gaus(0)",-Config::fitrange,Config::fitrange);
+    sub2->SetParameters(fit->GetParameter(3),fit->GetParameter(1),fit->GetParameter(4));
+
+    sub3 = new TF1("sub3","gaus(0)",-Config::fitrange,Config::fitrange);
+    sub3->SetParameters(fit->GetParameter(5),fit->GetParameter(1),fit->GetParameter(6));
+
+    sub3->SetLineColor(kGreen-3); // kViolet-3
+    sub3->SetLineWidth(2);
+    sub3->SetLineStyle(7);
+    sub3->Draw("same");
+  } 
+  
+  sub1->SetLineColor(kRed) ;  // kgreen-3
+  sub1->SetLineWidth(2);
+  sub1->SetLineStyle(7);
+  sub1->Draw("same");
+
+  sub2->SetLineColor(kBlue); // kViolet-3
+  sub2->SetLineWidth(2);
+  sub2->SetLineStyle(7);
+  sub2->Draw("same");
+}
+
+void drawStats(TF1 *& fit, TPaveText *& stats, TCanvas *& canv)
+{
+  canv->cd();
+  if (Config::formname.EqualTo("gaus1",TString::kExact)) 
+  {
+    stats = new TPaveText(0.6,0.78,0.9,0.88,"NDC");
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(0),fit->GetParameter(0),fit->GetParError(0)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(1),fit->GetParameter(1),fit->GetParError(1)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(2),fit->GetParameter(2),fit->GetParError(2)));
+    stats->SetTextSize(0.03);
+    stats->SetFillColorAlpha(kWhite,0.f);
+    stats->Draw("same");
+  }
+  else if (Config::formname.EqualTo("gaus2fm",TString::kExact)) {
+    stats = new TPaveText(0.6,0.68,0.9,0.88,"NDC");
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(0),fit->GetParameter(0),fit->GetParError(0)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(1),fit->GetParameter(1),fit->GetParError(1)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(2),fit->GetParameter(2),fit->GetParError(2)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(3),fit->GetParameter(3),fit->GetParError(3)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(4),fit->GetParameter(4),fit->GetParError(4)));
+    stats->SetTextSize(0.03);
+    stats->SetFillColorAlpha(kWhite,0.f);
+    stats->Draw("same");
+  } 
+  else if (Config::formname.EqualTo("gaus3fm",TString::kExact)) {
+    stats = new TPaveText(0.6,0.68,0.9,0.88,"NDC");
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(0),fit->GetParameter(0),fit->GetParError(0)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(1),fit->GetParameter(1),fit->GetParError(1)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(2),fit->GetParameter(2),fit->GetParError(2)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(3),fit->GetParameter(3),fit->GetParError(3)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(4),fit->GetParameter(4),fit->GetParError(4)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(5),fit->GetParameter(5),fit->GetParError(5)));
+    stats->AddText(Form("%s: %f #pm %f",fit->GetParName(6),fit->GetParameter(6),fit->GetParError(6)));
+    stats->SetTextSize(0.03);
+    stats->SetFillColorAlpha(kWhite,0.f);
+    stats->Draw("same");
+  } 
+}
+
+void deleteAll(TF1 *& fit, TF1 *& sub1, TF1 *& sub2, TF1 *& sub3, TPaveText *& text, TCanvas *& lincanv, TPaveText *& stats, TCanvas *& logcanv) 
+{
+  delete fit;
+  if (Config::formname.EqualTo("gaus2fm",TString::kExact) || Config::formname.EqualTo("gaus3fm",TString::kExact))
+  {
+    delete sub1;
+    delete sub2;
+    if (Config::formname.EqualTo("gaus3fm",TString::kExact)) 
+    {
+      delete sub3;
+    }
+  }
+  delete text;
+  delete lincanv;
+  delete stats;
+  delete logcanv;
 }
--- tuning_integral.C
+++ tuning_integral.C
@@ -12,76 +12,82 @@
 
 void tuning_integral()
 {
-  TString var   = "z";
-  Int_t   layer = 9;
+  TString var   = "phi";
   Float_t range = 6.0;
   Int_t   nbins = 1001;
 
   bool save = true;
-  ofstream yields(Form("zphi/%sintegrals.txt",var.Data()),std::ios_base::app);
+  ofstream yields(Form("zphi_newsimple/%sintegrals.txt",var.Data()),std::ios_base::app);
 
   TFile * file = TFile::Open("valtree.root");
   TTree * tree = (TTree*)file->Get("fittree");
-
-  TH1F * hist = new TH1F("hist","hist",nbins,-range,range); hist->Sumw2();
-  tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
-
-  int onesigbins[2]   = {-1,-1}; int onesigints[2]   = {-1,-1}; float onesigvals[2]   = {-99.,-99.}; bool onesigma   = false; 
-  int twosigbins[2]   = {-1,-1}; int twosigints[2]   = {-1,-1}; float twosigvals[2]   = {-99.,-99.}; bool twosigma   = false; 
-  int threesigbins[2] = {-1,-1}; int threesigints[2] = {-1,-1}; float threesigvals[2] = {-99.,-99.}; bool threesigma = false;
-
-  int total = hist->Integral(0,1002);
-  for (int i = 0; i <= 501; i++) {
-    int beg = 501 - i;
-    int end = 501 + i;
+  
+  for (int layer = 3; layer < 10; layer++)
+  {
+    TH1F * hist = new TH1F("hist","hist",nbins,-range,range); hist->Sumw2();
+    tree->Draw(Form("(%s_prop[%i]-%s_hit[%i])/sqrt(e%s_prop[%i])>>%s",var.Data(),layer,var.Data(),layer,var.Data(),layer,hist->GetName()),Form("%s_hit[%i]!=-1000",var.Data(),layer),"goff");
     
-    int integral = hist->Integral(beg,end);
-    if ((float(integral)/float(total) > 0.6826895) && !onesigma) {
-      onesigma = true;
-      onesigbins[0] = end - 1; onesigbins[1] = end;
-      onesigints[0] = hist->Integral(beg+1,end-1);
-      onesigints[1] = integral;
-      onesigvals[0] = hist->GetBinCenter(end-1);
-      onesigvals[1] = hist->GetBinCenter(end);
-    }
-    else if ((float(integral)/float(total) > 0.9544997) && !twosigma) {
-      twosigma = true;
-      twosigbins[0] = end - 1; twosigbins[1] = end;
-      twosigints[0] = hist->Integral(beg+1,end-1);
-      twosigints[1] = integral;
-      twosigvals[0] = hist->GetBinCenter(end-1);
-      twosigvals[1] = hist->GetBinCenter(end);
-    }
-    else if ((float(integral)/float(total) > 0.9973002) && !threesigma) {
-      threesigma = true;
-      threesigbins[0] = end - 1; threesigbins[1] = end;
-      threesigints[0] = hist->Integral(beg+1,end-1);
-      threesigints[1] = integral;
-      threesigvals[0] = hist->GetBinCenter(end-1);
-      threesigvals[1] = hist->GetBinCenter(end);
-    }
-  }
+    int onesigbins[2]   = {-1,-1}; int onesigints[2]   = {-1,-1}; float onesigvals[2]   = {-99.,-99.}; bool onesigma   = false; 
+    int twosigbins[2]   = {-1,-1}; int twosigints[2]   = {-1,-1}; float twosigvals[2]   = {-99.,-99.}; bool twosigma   = false; 
+    int threesigbins[2] = {-1,-1}; int threesigints[2] = {-1,-1}; float threesigvals[2] = {-99.,-99.}; bool threesigma = false;
 
-  if (!save) {
-    std::cout << "layer: " << layer << " range: " << range << std::endl;
-    std::cout << total << std::endl << std::endl;
-    std::cout << onesigbins[0] << "-" << onesigbins[1] << std::endl;
-    std::cout << onesigints[0] << "-" << onesigints[1] << std::endl;
-    std::cout << onesigvals[0] << "-" << onesigvals[1] << std::endl << std::endl;
+    int total = hist->Integral(0,1002);
+    for (int diff = 0; diff <= 501; diff++) {
+      int beg = 501 - diff;
+      int end = 501 + diff;
     
-    std::cout << twosigbins[0] << "-" << twosigbins[1] << std::endl;
-    std::cout << twosigints[0] << "-" << twosigints[1] << std::endl;
-    std::cout << twosigvals[0] << "-" << twosigvals[1] << std::endl << std::endl;
+      int integral = hist->Integral(beg,end);
+      if ((float(integral)/float(total) > 0.6826895) && !onesigma) {
+	onesigma = true;
+	onesigbins[0] = end - 1; onesigbins[1] = end;
+	onesigints[0] = hist->Integral(beg+1,end-1);
+	onesigints[1] = integral;
+	onesigvals[0] = hist->GetBinCenter(end-1);
+	onesigvals[1] = hist->GetBinCenter(end);
+      }
+      else if ((float(integral)/float(total) > 0.9544997) && !twosigma) {
+	twosigma = true;
+	twosigbins[0] = end - 1; twosigbins[1] = end;
+	twosigints[0] = hist->Integral(beg+1,end-1);
+	twosigints[1] = integral;
+	twosigvals[0] = hist->GetBinCenter(end-1);
+	twosigvals[1] = hist->GetBinCenter(end);
+      }
+      else if ((float(integral)/float(total) > 0.9973002) && !threesigma) {
+	threesigma = true;
+	threesigbins[0] = end - 1; threesigbins[1] = end;
+	threesigints[0] = hist->Integral(beg+1,end-1);
+	threesigints[1] = integral;
+	threesigvals[0] = hist->GetBinCenter(end-1);
+	threesigvals[1] = hist->GetBinCenter(end);
+      }
+    }
 
-    std::cout << threesigbins[0] << "-" << threesigbins[1] << std::endl;
-    std::cout << threesigints[0] << "-" << threesigints[1] << std::endl;
-    std::cout << threesigvals[0] << "-" << threesigvals[1] << std::endl;
+    if (!save) {
+      std::cout << "layer: " << layer << " range: " << range << std::endl;
+      std::cout << total << std::endl << std::endl;
+      std::cout << onesigbins[0] << "-" << onesigbins[1] << std::endl;
+      std::cout << onesigints[0] << "-" << onesigints[1] << std::endl;
+      std::cout << onesigvals[0] << "-" << onesigvals[1] << std::endl << std::endl;
+      
+      std::cout << twosigbins[0] << "-" << twosigbins[1] << std::endl;
+      std::cout << twosigints[0] << "-" << twosigints[1] << std::endl;
+      std::cout << twosigvals[0] << "-" << twosigvals[1] << std::endl << std::endl;
+      
+      std::cout << threesigbins[0] << "-" << threesigbins[1] << std::endl;
+      std::cout << threesigints[0] << "-" << threesigints[1] << std::endl;
+      std::cout << threesigvals[0] << "-" << threesigvals[1] << std::endl;
+    }
+    else {
+      yields << layer << " "
+	     << (onesigvals[0]+onesigvals[1])/2. << " "
+	     << (twosigvals[0]+twosigvals[1])/2. << " "
+	     << (threesigvals[0]+threesigvals[1])/2. << " "
+	     << std::endl;
+    }
+    delete hist;
   }
-  else {
-    yields << layer << " "
-	   << (onesigvals[0]+onesigvals[1])/2. << " "
-      	   << (twosigvals[0]+twosigvals[1])/2. << " "
-      	   << (threesigvals[0]+threesigvals[1])/2. << " "
-	   << std::endl;
-  }
+  delete tree;
+  delete file;
+  yields.close();
 }
