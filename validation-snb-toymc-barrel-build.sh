f3aa7c88d948b86d96d380a2c2159e21a11365a3
Kevin McDermott <kpm82@cornell.edu>
2016-11-16 09:07:51 -0800

    small fix

--- validation-snb-toymc-barrel-build.sh
+++ validation-snb-toymc-barrel-build.sh
@@ -6,10 +6,10 @@ make -j 12
 
 dir=/data/nfsmic/${USER}/tmp
 
-echo "snb toymc" nth=${nth} "BH (barrel): validation"
+echo "snb toymc BH (barrel): validation [nTH:24, nVU:8]"
 ./mkFit/mkFit --read --file-name ${dir}/simtracks_barrel_10x10k_val.bin --build-bh   --num-thr 24 >& log_snb_10x10k_BH_NVU8int_NTH24_val.txt
 mv valtree.root valtree_BH.root
-echo "snb toymc" nth=${nth} "COMB (barrel): validation"
+echo "snb toymc COMB (barrel): validation [nTH:24, nVU:8]"
 ./mkFit/mkFit --read --file-name ${dir}/simtracks_barrel_10x10k_val.bin --build-comb --num-thr 24 --cloner-single-thread >& log_snb_10x10k_COMB_NVU8int_NTH24_val.txt
 mv valtree.root valtree_COMB.root
 
